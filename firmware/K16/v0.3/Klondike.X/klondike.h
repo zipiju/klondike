1832e3469a3b2f109c7653155ad5963125a4b76d
zipiju <cibula@gamepub.sk>
2014-02-02 18:07:46 +0100

    Update and move firmware files
    Update K16 v1 firmware, add K16 v0.3 firmware to firmware dir

--- firmware/K16/v0.3/Klondike.X/klondike.h
+++ firmware/K16/v0.3/Klondike.X/klondike.h
@@ -26,17 +26,22 @@ extern "C" {
 #endif
 
 #define MASTER_ADDRESS      0x00
-#define MAX_WORK_COUNT      8   // must be binary multiple and match driver
+#define MAX_WORK_COUNT      4   // must be binary multiple and match driver
 #define WORKMASK            MAX_WORK_COUNT-1
 #define USB_RECORD_SIZE     15
 
 // set values for ASIC PLL, we use R=16 N=Freq in MHz
 #define WORK_TICKS          16384   // 2^32 total / 2^18 hashes per tick
-#define TICK_TOTAL          9598   // 2^18 / 21.33uS TMR0 period (adjusted down for push time)
-#define DEFAULT_HASHCLOCK   1200
+#define TICK_TOTAL          12283   // 2^18 / 21.33uS TMR0 period (adjusted down for push time)
+#define CLOCK_R_VALUE       16
+#define DEFAULT_HASHCLOCK   333
 #define CLOCK_NOCHG_MASK    0x00000002
-#define MIN_HASH_CLOCK      600
-#define MAX_HASH_CLOCK      1500
+#define CLOCK_LOW_CHG       0x00030007
+#define CLOCK_HALF_CHG      0x00030017
+#define CLOCK_HIGH_CFG      0x00000172
+#define MIN_HASH_CLOCK      128
+#define HALF_HASH_CLOCK     250
+#define MAX_HASH_CLOCK      450
 
 // default temp/fan values
 #define DEFAULT_TEMP_TARGET     143 // default temperature to hold by adjusting fan speed
@@ -113,7 +118,6 @@ void InitTempSensor(void);
 void InitWorkTick(void);
 void InitResultRx(void);
 
-void SetAsicClock(DWORD frequency);
 void SendAsicData(WORKTASK *work);
 void Send32(void);
 void AsicPreCalc(WORKTASK *work);
